a464790273aded0b687a659a0449dbe180a6a039
fchui <masil@live.ca>
2021-01-04 21:52:14 -0800

    Added Dialogue Function

---
+++ AlphaClass/Assets/Scripts/Dialogue/TestScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    public DialogueBase dialogue;
+    //  public bool active = false;
+
+    public void TriggerDialogue()
+    {
+        DialogueManager.instance.EnqueueDialogue(dialogue);
+    }
+
+    public void GetNextLine()
+    {
+        DialogueManager.instance.DequeueDialogue();
+        //  if (DialogueManager.instance.dialogueBox.activeSelf == false)
+        // {
+        //    active = false;
+        // }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && DialogueManager.instance.dialogueBox.activeSelf == true)
+        {
+            GetNextLine();
+            if (DialogueManager.instance.dialogueBox.activeSelf == false)
+            {
+                return;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && DialogueManager.instance.dialogueBox.activeSelf == false)
+        {
+            //    active = true;
+            TriggerDialogue();
+        }
+
+    }
+}
